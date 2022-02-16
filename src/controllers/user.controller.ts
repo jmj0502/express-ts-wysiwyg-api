e6351bab625edbec2dada594f2c060e451b20f12
Martin Juarez <juarezmachadojorgemartin@gmail.com>
2022-02-15 21:30:51 -0400

    Improvements: Now the UserController is technically done!

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,35 @@
+import {
+	Controller,
+	Get,
+	Post
+} from "../decorators/controller";
+import { UserService } from "../services/user.service";
+import { Request, Response } from "express";
+import { Service } from "typedi";
+import { empty } from "@prisma/client/runtime";
+
+@Controller("api/users")
+@Service()
+export class UserController {
+	constructor(
+		private userService: UserService
+	) {}
+
+	@Get("")
+	public async registerUser(req: Request, res: Response): Promise<Response> {
+		const result = await this.userService.createUser(req.body['email']);
+		if (!result) {
+			return res.status(500).json({success: false, message: "Something went wrong!"});
+		}
+		return res.status(201).json({success: true, user: result});
+	}
+
+	@Post("")
+	public async logIn(req: Request, res: Response): Promise<Response> {
+		const result = await this.userService.findUser(req.body['email']);
+		if (!result) {
+			return res.status(404).json({success: false, message: "Couldn't find the provided user."});
+		}
+		return res.status(200).json({success: false, user: result});
+	}
+}
